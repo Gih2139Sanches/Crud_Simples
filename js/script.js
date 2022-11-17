2a5f6f6e6d7445016459e3cfa7fc178a81d62019
Giovana Sanches <giovanaclemente2139@gmail.com>
2022-11-17 03:12:47 -0300

    login e cadastro

--- js/script.js
+++ js/script.js
@@ -6,3 +6,22 @@ function mostraMenu(){
 function escondeMenu(){
   navLinks.style.right = "-200px";
 }
+
+/* Tela de Login */
+const signupBtn = document.getElementById("signupBtn");
+const signinBtn = document.getElementById("signinBtn");
+const campoNome = document.getElementById("campoNome");
+const titulo = document.getElementById("titulo");
+
+signinBtn.onclick = function(){
+  campoNome.style.maxHeight = "0";
+  titulo.innerHTML = "Sign In";
+  signupBtn.classList.add("disabilitado");
+  signinBtn.classList.remove("disabilitado");
+}
+signupBtn.onclick = function(){
+  campoNome.style.maxHeight = "60px";
+  titulo.innerHTML = "Sign Up";
+  signupBtn.classList.remove("disabilitado");
+  signinBtn.classList.add("disabilitado");
+}
